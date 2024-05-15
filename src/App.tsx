9a8f030ac1b37862d93e3415b1de5c9a4a0fbd9c
zinping <haozinping@gmail.com>
2024-05-14 17:30:03 -0700

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  DAppKitProvider,
+  useConnex,
+  useWalletModal,
+  useWallet,
+} from "@vechain/dapp-kit-react";
+// import Counter from "./Counter";
+
+export default function App() {
+  return (
+    <DAppKitProvider
+      nodeUrl="https://node-testnet.vechain.energy"
+      genesis="test"
+      usePersistence
+    >
+      <center>
+        <Connections />
+        <hr />
+        <Counter />
+      </center>
+    </DAppKitProvider>
+  );
+}
+
+function Connections() {
+  const connex = useConnex();
+  const { account, disconnect } = useWallet();
+  const modal = useWalletModal();
+
+  if (account) {
+    return (
+      <button onClick={disconnect}>Disconnect as ..{account.slice(-6)}</button>
+    );
+  }
+
+  return <button onClick={modal.open}>Connect Wallet</button>;
+}
